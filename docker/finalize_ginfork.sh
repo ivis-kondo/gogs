eb46e5d3b7612e1679be467257203c8edd7bdcc0
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-09-08 10:13:46 +0900

    docker用のスクリプト

---
+++ docker/finalize_ginfork.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Finalize the build
+
+set -x
+set -e
+
+# Create git user for Gogs
+addgroup -system git
+adduser --ingroup git --no-create-home --disabled-password --gecos 'Gogs Git User' git --home /data/git -shell /bin/bash && usermod --password '*' git && passwd --unlock git
+echo "export GOGS_CUSTOM=${GOGS_CUSTOM}" >> /etc/profile
+
+# Final cleaning
+rm -rf /app/gogs/build
+rm /app/gogs/docker/finalize.sh
+rm /app/gogs/docker/nsswitch.conf
+rm /app/gogs/docker/README.md
