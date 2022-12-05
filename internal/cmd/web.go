6b4dfaf559f2cad5af08dfc88f398398f76968b8
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-05 10:43:15 +0900

    app.iniから正しく設定されいるか確認

--- internal/cmd/web.go
+++ internal/cmd/web.go
@@ -167,8 +167,8 @@ func runWeb(c *cli.Context) error {
 	reqSignIn := context.Toggle(&context.ToggleOptions{SignInRequired: true})
 	//後で消すこと　by tsukioka
 	log.Trace("[RCOS LOG in runWeb] RequireSigninView : %v", conf.Auth.RequireSigninView)
-	//ignSignIn := context.Toggle(&context.ToggleOptions{SignInRequired: conf.Auth.RequireSigninView})
-	ignSignIn := context.Toggle(&context.ToggleOptions{SignInRequired: true})
+	ignSignIn := context.Toggle(&context.ToggleOptions{SignInRequired: conf.Auth.RequireSigninView})
+	//ignSignIn := context.Toggle(&context.ToggleOptions{SignInRequired: true})
 	reqSignOut := context.Toggle(&context.ToggleOptions{SignOutRequired: true})
 
 	bindIgnErr := binding.BindIgnErr
