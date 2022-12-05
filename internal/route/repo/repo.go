cbed9773b9d89ba4eea2eb90e32436546bcd518c
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-05 09:08:18 +0900

    isRequireSigninViewの値の確認

--- internal/route/repo/repo.go
+++ internal/route/repo/repo.go
@@ -271,6 +271,8 @@ func Action(c *context.Context) {
 }
 
 func Download(c *context.Context) {
+	isRequireSigninView := conf.Auth.RequireSigninView
+	log.Trace("[Debug log RCOS] isRequireSigninView : %v", isRequireSigninView)
 	var (
 		uri           = c.Params("*")
 		refName       string
