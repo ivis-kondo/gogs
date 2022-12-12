16ec502c318b8451c72c6fe7ccdb6996682a5ba7
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-12 12:41:30 +0900

    Download のlogをコメントアウト

--- internal/route/repo/repo.go
+++ internal/route/repo/repo.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"github.com/unknwon/com"
@@ -295,7 +294,8 @@ func Download(c *context.Context) {
 	}
 	refName = strings.TrimSuffix(uri, ext)
 
-	log.Info("Downloaded " + c.Repo.Repository.Name + "-" + refName + ext + " by " + c.User.Name + "(total: " + strconv.FormatUint(c.Repo.Repository.Downloaded, 10) + " downloaded)")
+	//★
+	//log.Info("Downloaded " + c.Repo.Repository.Name + "-" + refName + ext + " by " + c.User.Name + "(total: " + strconv.FormatUint(c.Repo.Repository.Downloaded, 10) + " downloaded)")
 
 	if !com.IsDir(archivePath) {
 		if err := os.MkdirAll(archivePath, os.ModePerm); err != nil {
