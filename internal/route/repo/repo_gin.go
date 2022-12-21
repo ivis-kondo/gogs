19db543748251a54706614a8795bf17f2004009d
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-21 09:57:31 +0900

    認証エラー、ratelimit超過エラーのフォロー

--- internal/route/repo/repo_gin.go
+++ internal/route/repo/repo_gin.go
@@ -245,6 +245,10 @@ func (f repoUtil) fetchContentsOnGithub(blobPath string) ([]byte, error) {
 	}
 	if resp.StatusCode == http.StatusNotFound {
 		return nil, fmt.Errorf("Error: blob not found.")
+	} else if resp.StatusCode == http.StatusUnauthorized {
+		return nil, fmt.Errorf("Failure Authorization bacause Github API Token is invalid")
+	} else if resp.StatusCode == http.StatusForbidden {
+		return nil, fmt.Errorf("Failure Request for GitHub bacause Github API rate limit exceeded")
 	}
 	defer resp.Body.Close()
 
