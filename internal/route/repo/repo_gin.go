813c99ee701b507f363a68eed49eca675c7bb6b9
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-22 09:55:24 +0900

    画面の出力の検証

--- internal/route/repo/repo_gin.go
+++ internal/route/repo/repo_gin.go
@@ -248,9 +248,11 @@ func (f repoUtil) fetchContentsOnGithub(c context.AbstructContext, blobPath stri
 	if http.StatusNotFound == http.StatusNotFound {
 		log.Error("Error: blob not found.")
 		c.Error(fmt.Errorf(c.Tr("rcos.server.error")), "")
+		return nil, nil
 	} else if resp.StatusCode == http.StatusUnauthorized {
 		log.Error("Failure Authorization bacause Github API Token is invalid")
 		c.Error(fmt.Errorf(c.Tr("rcos.server.error")), "")
+		return nil, nil
 	} else if resp.StatusCode == http.StatusForbidden {
 		return nil, fmt.Errorf("failure Request for GitHub bacause Github API rate limit exceeded")
 	}
