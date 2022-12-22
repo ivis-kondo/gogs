1a6871b7d8b8573dc414fdbd4c0eaff3dc0b77e0
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-12-22 13:37:59 +0900

    token取得確認

--- internal/route/repo/repo_gin.go
+++ internal/route/repo/repo_gin.go
@@ -219,6 +219,7 @@ type AbstructRepoUtil interface {
 type repoUtil func()
 
 func (f repoUtil) FetchContentsOnGithub(c context.AbstructContext, blobPath string) ([]byte, error) {
+	log.Trace("githun api token : %s", conf.GitHub.ApiToken)
 	return f.fetchContentsOnGithub(c, blobPath, conf.GitHub.ApiToken)
 }
 
