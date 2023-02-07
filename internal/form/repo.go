15afcc13ab474349f312e801e84c36e76fe74174
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-02-07 11:39:34 +0900

    fix create repo

--- internal/form/repo.go
+++ internal/form/repo.go
@@ -23,15 +23,17 @@ import (
 //         \/        \/                   \/        \/                        \/       \/ \/
 
 type CreateRepo struct {
-	UserID      int64  `binding:"Required"`
-	RepoName    string `binding:"Required;AlphaDashDot;MaxSize(100)"`
-	Private     bool
-	Unlisted    bool
-	Description string `binding:"MaxSize(512)"`
-	AutoInit    bool
-	Gitignores  string
-	License     string
-	Readme      string
+	UserID             int64  `binding:"Required"`
+	RepoName           string `binding:"Required;AlphaDashDot;MaxSize(100)"`
+	Private            bool
+	Unlisted           bool
+	Description        string `binding:"MaxSize(512)"`
+	AutoInit           bool
+	Gitignores         string
+	License            string
+	Readme             string
+	ProjectName        string
+	ProjectDescription string
 }
 
 func (f *CreateRepo) Validate(ctx *macaron.Context, errs binding.Errors) binding.Errors {
