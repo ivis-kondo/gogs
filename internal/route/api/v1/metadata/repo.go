09093987388cfa957e0c2ed4c3846082f926e9cf
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-03-03 17:17:06 +0900

    complete check

--- internal/route/api/v1/metadata/repo.go
+++ internal/route/api/v1/metadata/repo.go
@@ -149,9 +149,9 @@ func GetAllMetadataByRepoIDAndBranch(c *context.APIContext) {
 		})
 		return
 	}
-	gin_monitoring.DatasetStructure = ""
+
 	if gin_monitoring.DatasetStructure == "" {
-		c.JSON(999, map[string]interface{}{
+		c.JSON(http.StatusNotFound, map[string]interface{}{
 			"message": fmt.Sprintf("This repository <ID : %s, Name: %s> dosen't have Research Policy. Please 'ADD DMP' on Gin-fork UI", repoid_str, repo.FullName()),
 		})
 		return
