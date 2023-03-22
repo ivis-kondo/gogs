4ae74fb5a2de6816193456c5081d90eb8d355c09
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-03-22 09:39:59 +0900

    add debug

--- internal/db/repo_metadata.go
+++ internal/db/repo_metadata.go
@@ -148,7 +148,7 @@ func ExtractExperimentPackageList(struct_type string, datasets []datastruct.Data
 						parameterExperimentList = append(parameterExperimentList, dataset.ID)
 					}
 				}
-
+				log.Trace("[ExtractExperimentPackageList()] path_compoment[len(path_compoment)-1] : %s ", path_compoment[len(path_compoment)-1])
 				if !isInvoled && const_utils.IsParameterFolder(path_compoment[len(path_compoment)-1]) {
 					log.Trace("[ExtractExperimentPackageList()] dataset.ID(Path) : %s add to parameterExperimentList", dataset.ID)
 					parameterExperimentList = append(parameterExperimentList, dataset.ID)
