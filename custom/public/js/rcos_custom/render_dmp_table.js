0543c951fddeaaad6a9524740e393f89b6faf21d
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-06-04 16:58:23 +0900

    fix dmp render

--- custom/public/js/rcos_custom/render_dmp_table.js
+++ custom/public/js/rcos_custom/render_dmp_table.js
@@ -31,6 +31,48 @@
 //     createTable(tableEle, items)
 // });
 
+function CreateTableFromJSON(tbodyEle, dmp) {
+    let rowNum = getRowNum(dmp) //行数
+    let colNum = getColNum(dmp) //列数
+
+    // create reseted
+    let dmpData2DArr = createDmp2dArr(rowNum, colNum)
+    let dmptable = fillDmp2dArr(dmpData2DArr, dmp, 0, 0)
+    let colspanTable = fillColspan2DArr(dmptable, rowNum, colNum)
+    let rowspanTable = fillRowspan2DArr(dmptable, colspanTable, rowNum, colNum)
+
+    for (let i =0; i < rowNum; i++)  {
+        let tr = document.createElement("tr");
+        for (let j = 0; j < colNum; j++) {
+            let dmpUnitVal = dmptable[i][j]
+            if (dmpUnitVal != ""){
+                let td = document.createElement("td");
+                td.innerHTML = dmpUnitVal
+                let colspanNum = colspanTable[i][j]
+                if (colspanNum >1) {
+                    td.colSpan = colspanNum
+                }
+                let rowspanNum = rowspanTable[i][j]
+                if (rowspanNum >1) {
+                    td.rowSpan = rowspanNum
+                }
+            }
+            tr.appendChild(td)
+        }
+        tbodyEle.appendChild(tr)
+    }
+
+
+
+}
+
+$(document).ready(function () {
+    let tbodyEle = document.getElementById("dmp");
+    let items = JSON.parse($('#items').val());
+    (document.getElementById("title")).innerHTML = "dmp.json (" + items.schema + ")";
+    CreateTableFromJSON(tbodyEle, items)
+});
+
 function outputLog(msg) {
     const fs = require("fs");
     log_msg = String(msg) + '\n'
@@ -42,6 +84,22 @@ function outputLog(msg) {
 
 function IsArrayObject(data)  {
     return Array.isArray(data)
+}
+
+function IsObjectInArrayObject(data)  {
+    if (IsArrayObject(data)){
+        if (data.length>0) {
+            if (IsObject(data[0])){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+    }else{
+        return false
+    }
 }
 
 function IsObject(data)  {
@@ -75,63 +133,38 @@ function createDmp2dArr(rowNum, colNum) {
 }
 
 function fillDmp2dArr(dmp2DArr, dmp, raw_index, col_index) {
-    console.log('fillDmp2dArr start')
-    console.log('raw_index :' + raw_index)
-    console.log('col_index :' + col_index)
     // fill DMP data 2d Array
     for (const key in dmp) {
-        console.log('key_1 :' + key)
         const value = dmp[key]
-        console.log('value :' + value)
-        console.log('value type :' + typeof value)
         const record_num =  getLeafNum(value)
-        console.log('record_num :' + record_num)
-        console.log('raw_index :' + raw_index)
-        endPoint = raw_index + record_num
-        console.log('endPoint :' + endPoint)
 
+        endPoint = raw_index
+        if (IsObjectInArrayObject(value)) {
+            endPoint = endPoint + record_num
+        }else {
+            endPoint = endPoint + 1
+        }
         for (i =raw_index; i< endPoint; i++) {
-            console.log('dmp2DArr[' + i + '][' + col_index + '] = ' + key)
             dmp2DArr[i][col_index] = key
-            console.log('dmp2DArr['+ i +'] :' + dmp2DArr[i])
             if (record_num == 1) {
-                console.log('dmp2DArr[' + i + '][' + (col_index+1) + '] = ' + String(value))
                 dmp2DArr[i][col_index+1] = String(value)
-                console.log('dmp2DArr['+ i +'] :' + dmp2DArr[i])
             }else{
                 if (IsArrayObject(value)) {
                     // is array
-                    console.log('Is Array!!!!!')
-                    console.log('value.length :' + value.length)
-                    console.log('value :' + value)
-                    console.log('value :' + JSON.stringify(value))
                     let arr_start_index = i
-                    console.log('PRE arr_start_index :' + arr_start_index)
                     for (let nested_i = 0; nested_i < value.length; ++nested_i) {
-                        console.log('nested_i :' + nested_i)
-                        console.log('value.length :' + value.length)
                         const arr_data = value[nested_i]
-                        console.log('arr_data :' + arr_data)
-                        console.log('arr_data :' + JSON.stringify(arr_data))
                         if (IsObject(arr_data)) {
-                            console.log('Is Obj!!!!!')
                             const arr_data_record_num =  getLeafNum(arr_data)
-                            console.log('arr_data_record_num :' + arr_data_record_num)
-                            console.log('dmp2DArr[' + arr_start_index + '][' + (col_index+1) + '] = ' + nested_i)
-                            dmp2DArr[arr_start_index][col_index+1] = nested_i
+                            dmp2DArr[arr_start_index][col_index+1] = String(nested_i)
                             next_raw_index = arr_start_index
                             next_col_index = col_index+2
                             dmp2DArr = fillDmp2dArr(dmp2DArr, arr_data, next_raw_index, next_col_index)
-                            console.log('ADD arr_start_index :' + arr_start_index + '+' + arr_data_record_num)
                             arr_start_index = arr_start_index + arr_data_record_num
-                            console.log('CHANGE arr_start_index :' + arr_start_index)
                         } else {
-                            console.log('Is Not Obj!!!!!')
-                            dmp2DArr[arr_start_index][col_index+1] = nested_i
+                            dmp2DArr[arr_start_index][col_index+1] = String(nested_i)
                             dmp2DArr[arr_start_index][col_index+2] = arr_data
-                            console.log('ADD arr_start_index :' + arr_start_index + '+' + 1)
                             arr_start_index = arr_start_index + 1
-                            console.log('CHANGE arr_start_index :' + arr_start_index)
                         }
                     }
 
@@ -143,14 +176,7 @@ function fillDmp2dArr(dmp2DArr, dmp, raw_index, col_index) {
 
             }
         }
-
         raw_index = raw_index + record_num
-        console.log('changed raw_index :' + raw_index)
-    }
-
-    console.log('dmp2DArr.length :' + dmp2DArr.length)
-    for (let i = 0; i < dmp2DArr.length; ++i) {
-        console.log(dmp2DArr[i]);
     }
     return dmp2DArr
 }
@@ -171,6 +197,99 @@ function createRowspanOrColspan2DArr(rowNum, colNum) {
     // }
     return rowspanOrColspan2DArr
 }
+
+function isEmptyInNextCol(arr, index){
+    const arrLength = arr.length
+    const nextIndex = index+1
+    if (arrLength==nextIndex) {
+        return false
+    }else {
+        const nextVal = arr[nextIndex]
+        if (nextVal==""){
+            return true
+        }else{
+            return false
+        }
+    }
+
+}
+
+function fillColspan2DArr(dmpDataTable, rowNum, colNum) {
+    let colspan2DArr =createRowspanOrColspan2DArr(rowNum, colNum)
+
+    for (let i=0; i < rowNum; i++) {
+        //console.log('i : ' + i)
+        let rowDmpData = dmpDataTable[i]
+        //console.log('rowDmpData : ' + rowDmpData)
+        for (let j = 0; j < colNum; j++) {
+            //console.log('j : ' + j)
+            if (rowDmpData[j]==""){
+                continue
+            }else{
+                // console.log('isEmptyInNextCol : ' + isEmptyInNextCol(rowDmpData, j))
+                if (isEmptyInNextCol(rowDmpData, j)){
+                    let colspanNum = colNum - j
+                    //console.log('colspanNum = ' + colNum + ' - ', j, '+1')
+                    //console.log('colspanNum : ' + colspanNum)
+                    colspan2DArr[i][j] = colspanNum
+                    //console.log('colspan2DArr[' + i+']['+j+'] = ' + colspanNum)
+                    break
+                }else{
+                    continue
+                }
+            }
+
+        }
+    }
+    return colspan2DArr
+}
+
+function hasColspanBefore(colspanDataArry, targetIndex){
+    for (let i=0; i < targetIndex; i++) {
+        if (colspanDataArry[i]==0){
+            continue
+        }else{
+            return true
+        }
+    }
+    return false
+}
+
+function getRowspan(dmpDataTable, colspanTable, startRowIndex, colIndex) {
+    let rowspanNum = 0
+    for (let i=startRowIndex; i < dmpDataTable.length; i++) {
+        let targetVal = dmpDataTable[i][colIndex]
+        if (targetVal==""){
+            //check colspanTable
+            if (hasColspanBefore(colspanTable[i], colIndex)){
+                return rowspanNum+1
+            }
+            rowspanNum = rowspanNum +1
+        }else{
+            break
+        }
+    }
+    return rowspanNum +1
+}
+
+function fillRowspan2DArr(dmpDataTable, colspanTable, rowNum, colNum) {
+    let rowspan2DArr =createRowspanOrColspan2DArr(rowNum, colNum)
+    for (let i=0; i < rowNum; i++) {
+        let rowDmpData = dmpDataTable[i]
+        for (let j = 0; j < colNum; j++) {
+            let arrVal = rowDmpData[j]
+            if (arrVal==""){
+                continue
+            }else{
+                rowspanNum = getRowspan(dmpDataTable, colspanTable, i+1, j)
+                rowspan2DArr[i][j] = rowspanNum
+            }
+        }
+    }
+    return rowspan2DArr
+}
+
+
 
 
 
@@ -259,21 +378,7 @@ function getNestedNum(data, pre_nested_num) {
 }
 
 
-function CreateTableFromJSON(tbodyEle, dmp) {
-    let rowNum = getRowNum(dmp) //行数
-    let colNum = getColNum(dmp) //列数
 
-    // create reseted 3
-    dmpData2DArr = createDmp2dArr(rowNum, colNum)
-
-}
-
-// $(document).ready(function () {
-//     let tbodyEle = document.getElementById("dmp");
-//     let items = JSON.parse($('#items').val());
-//     (document.getElementById("title")).innerHTML = "dmp.json (" + items.schema + ")";
-//     CreateTableFromJSON(tbodyEle, items)
-// });
 
 
 
@@ -337,6 +442,17 @@ function CreateTableFromJSON(tbodyEle, dmp) {
 //     outputLog(dmptable[i]);
 //     console.log(dmptable[i]);
 // }
+// let colspanTable = fillColspan2DArr(dmptable, rowNum, colNum)
+// for (let i = 0; i < colspanTable.length; ++i) {
+//     outputLog(colspanTable[i]);
+//     console.log(colspanTable[i]);
+// }
+// let rawspanTable = fillRowspan2DArr(dmptable, colspanTable, rowNum, colNum)
+// console.log("rawspanTable");
+// for (let i = 0; i < rawspanTable.length; ++i) {
+//     outputLog(rawspanTable[i]);
+//     console.log(rawspanTable[i]);
+// }
 
 //meti ダメ
 // 32 行OK
@@ -366,33 +482,56 @@ function CreateTableFromJSON(tbodyEle, dmp) {
 //     outputLog(dmptable[i]);
 //     console.log(dmptable[i]);
 // }
+// let colspanTable = fillColspan2DArr(dmptable, rowNum, colNum)
+// console.log("colspanTable");
+// for (let i = 0; i < colspanTable.length; ++i) {
+//     outputLog(colspanTable[i]);
+//     console.log(colspanTable[i]);
+// }
+// let rawspanTable = fillRowspan2DArr(dmptable, colspanTable, rowNum, colNum)
+// console.log("rawspanTable");
+// for (let i = 0; i < rawspanTable.length; ++i) {
+//     outputLog(rawspanTable[i]);
+//     console.log(rawspanTable[i]);
+// }
 
 // jst
 // 44行 OK
 // 5列 OK
 // table OK
-var min = '{"workflowIdentifier": "basic","contentSize": "1GB","datasetStructure": "with_code","useDocker": "YES","schema": "jst","createDate": "2021/07/21","amedNumber": "0000","project": {"fiscalYear": 2021,"title": "The Project","problemName": "hogehoge","representative": {"belongTo": "NII","post": "hogehoge","name": "John Doe"}},"forPublication": {"hasUsed": false,"unwriteReason": "This is just content when \'hasUsed\' is true"},"researches": [{"title": "The Title","type": ["ヒト個人（研究参加者及びヒト試料由来のデータ）", "hoge_type"],"description": "hogehoge","releasePolicy": "hogehoge","concealReason": "hogehoge","hasOfferPolicy": false,"policyName": "This is just content when \'hasOfferPolicy\' is true","repositoryType": "hogehoge","repositoryName": "hogehoge","dataAmount": "hogehoge","dataSchema": "Excel","processingPolicy": "hogehoge","isRegistered": false,"registeredInfo": "hogehoge"}],"researchers": {"numberOfPeople": 1,"manager": {"isConcurrent": false,"personal": {"belongTo": "NII","post": "hogehoge","name": "Bill Doe"}},"staff": [{"belongTo": "NII","post": "hogehoge","name": "Mary Doe","e-Rad": "0000","canPublished": false,"postType": "Professor","financialResource": "AMED","employmentStatus": "full-time","roles": "data curation","remarks": "hogehoge"}]}}'
-outputLog('テスト JST')
-var min_data = JSON.parse(min);
-var rowNum = getRowNum(min_data) //行数
-outputLog('rowNum :' + rowNum)
-console.log('rowNum :' + rowNum);
-var colNum = getColNum(min_data) //列数
-outputLog('colNum :' + colNum)
-console.log('colNum :' + colNum);
-var dmpData2dArr_1 = createDmp2dArr(rowNum, colNum)
-outputLog('dmpData2dArr.length :' + dmpData2dArr_1.length)
-for (let i = 0; i < dmpData2dArr_1.length; ++i) {
-    outputLog(dmpData2dArr_1[i]);
-}
-dmptable = fillDmp2dArr(dmpData2dArr_1, min_data, 0, 0)
-outputLog('dmptable.length :' + dmptable.length)
-console.log('dmptable.length :' + dmptable.length);
-outputLog('finish fillDmp2dArr')
-for (let i = 0; i < dmptable.length; ++i) {
-    console.log(dmptable[i]);
-    outputLog(dmptable[i]);
-}
+// var min = '{"workflowIdentifier": "basic","contentSize": "1GB","datasetStructure": "with_code","useDocker": "YES","schema": "jst","createDate": "2021/07/21","amedNumber": "0000","project": {"fiscalYear": 2021,"title": "The Project","problemName": "hogehoge","representative": {"belongTo": "NII","post": "hogehoge","name": "John Doe"}},"forPublication": {"hasUsed": false,"unwriteReason": "This is just content when \'hasUsed\' is true"},"researches": [{"title": "The Title","type": ["hogehoge_type_1"],"description": "hogehoge","releasePolicy": "hogehoge","concealReason": "hogehoge","hasOfferPolicy": false,"policyName": "This is just content when \'hasOfferPolicy\' is true","repositoryType": "hogehoge","repositoryName": "hogehoge","dataAmount": "hogehoge","dataSchema": "Excel","processingPolicy": "hogehoge","isRegistered": false,"registeredInfo": "hogehoge"},{"title": "The Title_hogehoge_2","type": ["hogehoge_type_1"],"description": "hogehoge","releasePolicy": "hogehoge","concealReason": "hogehoge","hasOfferPolicy": false,"policyName": "This is just content when \'hasOfferPolicy\' is true","repositoryType": "hogehoge","repositoryName": "hogehoge","dataAmount": "hogehoge","dataSchema": "Excel","processingPolicy": "hogehoge","isRegistered": false,"registeredInfo": "hogehoge"}],"researchers": {"numberOfPeople": 1,"manager": {"isConcurrent": false,"personal": {"belongTo": "NII","post": "hogehoge","name": "Bill Doe"}},"staff": [{"belongTo": "NII","post": "hogehoge","name": "Mary Doe","e-Rad": "0000","canPublished": false,"postType": "Professor","financialResource": "AMED","employmentStatus": "full-time","roles": "data curation","remarks": "hogehoge"},{"belongTo": "NII","post": "hogehoge_post_2","name": "Mary Doe","e-Rad": "0000","canPublished": false,"postType": "Professor","financialResource": "AMED","employmentStatus": "full-time","roles": "data curation","remarks": "hogehoge"}]}}'
+// outputLog('テスト JST')
+// var min_data = JSON.parse(min);
+// var rowNum = getRowNum(min_data) //行数
+// outputLog('rowNum :' + rowNum)
+// console.log('rowNum :' + rowNum);
+// var colNum = getColNum(min_data) //列数
+// outputLog('colNum :' + colNum)
+// console.log('colNum :' + colNum);
+// var dmpData2dArr_1 = createDmp2dArr(rowNum, colNum)
+// outputLog('dmpData2dArr.length :' + dmpData2dArr_1.length)
+// for (let i = 0; i < dmpData2dArr_1.length; ++i) {
+//     outputLog(dmpData2dArr_1[i]);
+// }
+// dmptable = fillDmp2dArr(dmpData2dArr_1, min_data, 0, 0)
+// outputLog('dmptable.length :' + dmptable.length)
+// console.log('dmptable.length :' + dmptable.length);
+// outputLog('finish fillDmp2dArr')
+// for (let i = 0; i < dmptable.length; ++i) {
+//     console.log(dmptable[i]);
+//     outputLog(dmptable[i]);
+// }
+// let colspanTable = fillColspan2DArr(dmptable, rowNum, colNum)
+// for (let i = 0; i < colspanTable.length; ++i) {
+//     outputLog(colspanTable[i]);
+//     console.log(colspanTable[i]);
+// }
+// let rawspanTable = fillRowspan2DArr(dmptable, colspanTable, rowNum, colNum)
+// console.log("rawspanTable");
+// for (let i = 0; i < rawspanTable.length; ++i) {
+//     outputLog(rawspanTable[i]);
+//     console.log(rawspanTable[i]);
+// }
 
 // moon
 // 29行
@@ -419,4 +558,15 @@ for (let i = 0; i < dmptable.length; ++i) {
 // for (let i = 0; i < dmptable.length; ++i) {
 //     outputLog(dmptable[i]);
 //     console.log(dmptable[i]);
+// }
+// let colspanTable = fillColspan2DArr(dmptable, rowNum, colNum)
+// for (let i = 0; i < colspanTable.length; ++i) {
+//     outputLog(colspanTable[i]);
+//     console.log(colspanTable[i]);
+// }
+// let rawspanTable = fillRowspan2DArr(dmptable, colspanTable, rowNum, colNum)
+// console.log("rawspanTable");
+// for (let i = 0; i < rawspanTable.length; ++i) {
+//     outputLog(rawspanTable[i]);
+//     console.log(rawspanTable[i]);
 // }
