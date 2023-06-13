f3f2888be9d8066c0f7e23120e7e392d04f1295c
ivis-mizuguchi <rino.mizuguchi@ivis.co.jp>
2023-06-13 19:27:32 +0900

    新規タブで開く場合は、noopener属性にする

--- public/plugins/jquery.datetimepicker-2.4.5/jquery.datetimepicker.js
+++ public/plugins/jquery.datetimepicker-2.4.5/jquery.datetimepicker.js
@@ -765,7 +765,7 @@
 
 		createDateTimePicker = function (input) {
 			var datetimepicker = $('<div class="xdsoft_datetimepicker xdsoft_noselect"></div>'),
-				xdsoft_copyright = $('<div class="xdsoft_copyright"><a target="_blank" href="http://xdsoft.net/jqplugins/datetimepicker/">xdsoft.net</a></div>'),
+				xdsoft_copyright = $('<div class="xdsoft_copyright"><a target="_blank" rel="noopener" href="http://xdsoft.net/jqplugins/datetimepicker/">xdsoft.net</a></div>'),
 				datepicker = $('<div class="xdsoft_datepicker active"></div>'),
 				mounth_picker = $('<div class="xdsoft_mounthpicker"><button type="button" class="xdsoft_prev"></button><button type="button" class="xdsoft_today_button"></button>' +
 					'<div class="xdsoft_label xdsoft_month"><span></span><i></i></div>' +
