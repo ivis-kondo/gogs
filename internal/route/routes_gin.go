771a56add38cf75fd34e88b03b0e95b39795d0cc
ivis-futagami <ryota.futagami@ivis.co.jp>
2023-06-14 18:15:53 +0900

    fix filter

--- internal/route/routes_gin.go
+++ internal/route/routes_gin.go
@@ -16,19 +16,8 @@ func filterUnlistedRepos(repos []*db.Repository) []*db.Repository {
 func filterRepos(userID int64, repos []*db.Repository) []*db.Repository {
 	var showRep []*db.Repository
 	for _, repo := range repos {
-		if repo.Owner.IsOrganization() {
-			if repo.Owner.IsOrgMember(userID) {
-				showRep = append(showRep, repo)
-			}
-		} else {
-			// make repo private temporary
-			tmpIsPrivate := repo.IsPrivate
-			repo.IsPrivate = true
-			// Authorize
-			if db.Perms.Authorize(userID, repo, db.AccessModeRead) {
-				repo.IsPrivate = tmpIsPrivate
-				showRep = append(showRep, repo)
-			}
+		if repo.OwnerID == userID {
+			showRep = append(showRep, repo)
 		}
 	}
 	return showRep
@@ -37,14 +26,36 @@ func filterRepos(userID int64, repos []*db.Repository) []*db.Repository {
 func filterUsers(userID int64, users []*db.User) []*db.User {
 	var showUser []*db.User
 	for _, user := range users {
+		// 組織の場合、自分が所属していれば表示する
 		if user.IsOrganization() {
-			// show only belonging organization
 			if user.IsOrgMember(userID) {
 				showUser = append(showUser, user)
 			}
-		} else {
-			// show only user itself
-			if user.ID == userID {
+		} else { // ユーザーの場合
+			isShow := map[int64]bool{}
+			isShow[userID] = true
+
+			repos, _ := db.GetUserAndCollaborativeRepositories(userID)
+			for _, repo := range repos {
+				// 自分が所有するリポジトリの共同編集者は表示する
+				if repo.OwnerID == userID {
+					collaborators, _ := repo.GetCollaborators()
+					for _, collaborator := range collaborators {
+						isShow[collaborator.User.ID] = true
+					}
+				} else { // 自分が共同編集しているリポジトリの所有者は表示する
+					isShow[repo.OwnerID] = true
+				}
+			}
+
+			// 自分と同じ組織に所属しているユーザーは表示する
+			orgs, _ := db.GetOrgsByUserID(userID, true)
+			for _, org := range orgs {
+				if org.IsOrgMember(user.ID) {
+					isShow[user.ID] = true
+				}
+			}
+			if isShow[user.ID] == true {
 				showUser = append(showUser, user)
 			}
 		}
