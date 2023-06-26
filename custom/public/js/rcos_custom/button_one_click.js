161b625c3647a16635bbe1056e6bddb6b8107948
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-06-26 16:22:41 +0900

    remove log in JS

--- custom/public/js/rcos_custom/button_one_click.js
+++ custom/public/js/rcos_custom/button_one_click.js
@@ -3,10 +3,8 @@ var dblClickFlag = null;
 function PreventionDbClick() {
     if (dblClickFlag == null) {
         dblClickFlag = 1;
-        console.log('PreventionDbClick() True');
         return true;
     } else {
-        console.log('PreventionDbClick() Flase');
         return false;
     }
 }
