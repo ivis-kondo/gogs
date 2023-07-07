79ab0db504da658881b377976bca988710f4d207
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-07-07 14:08:20 +0900

    fix tabel

--- internal/db/access_tokens.go
+++ internal/db/access_tokens.go
@@ -47,7 +47,7 @@ type AccessToken struct {
 	UserID     int64 `xorm:"uid INDEX" gorm:"COLUMN:uid;INDEX"`
 	Name       string
 	Sha1       string `xorm:"UNIQUE VARCHAR(40)" gorm:"TYPE:VARCHAR(40);UNIQUE"`
-	ExpireUnix int64
+	ExpireUnix int64  `xorm:"expire_unix" gorm:"COLUMN:expire_unix"`
 
 	Created           time.Time `xorm:"-" gorm:"-" json:"-"`
 	CreatedUnix       int64
